0db2b1087e88cc7fd72402f3c73d0f63f4a45c3b
Ostap Maliuvanchuk <ostap@salemove.com>
2017-10-02 22:07:19 +0300

    Add exersice 1.40
    Function as return value

---
+++ exercise-1.40.scm
@@ -0,0 +1,42 @@
+#lang racket
+
+
+(define dx 0.00001)
+
+(define (deriv g)
+  (lambda (x)
+    (/ (- (g (+ x dx)) (g x))
+       dx)))
+
+(define (newton-transform g)
+  (lambda (x)
+    (- x (/ (g x)
+            ((deriv g) x)))))
+
+(define tolerance 0.00001)
+
+(define (fixed-point f first-guess)
+  (define (close-enough? v1 v2)
+    (< (abs (- v1 v2))
+       tolerance))
+  (define (try guess)
+    (let ((next (f guess)))
+      (if (close-enough? guess next)
+          next
+          (try next))))
+  (try first-guess))
+
+(define (newtons-method g guess)
+  (fixed-point (newton-transform g)
+               guess))
+
+(define (square a) (* a a))
+(define (cube a) (* a a a))
+
+(define (cubic a b c)
+  (lambda (x) (+ (cube x)
+                (* a (square x))
+                (* b x)
+                c)))
+
+(newtons-method (cubic 1 2 3) 1)
